25baf39a64361ac91ea2d15a5b6614ce0d8108a0
Claudia Hernández <claudiacarril8@gmail.com>
2022-12-05 17:45:18 +0100

    Index.js - Add new transaction

--- index.js
+++ index.js
@@ -1,2 +1,41 @@
 // Guardar los gatos e ingresos como objetos:
 // "Nomina": 1000€,.... (JSON.stringify)
+
+
+// CALCULO "TU AHORRO"
+
+
+
+// MOSTRAR INGRESOS Y GASTOS
+
+
+
+
+// HISTORIAL
+function showMovements() {
+    const movementsRecord = document.getElementById("movements-record")
+    const accountMovements = document.createElement('p')
+    movementsRecord.appendChild(accountMovements)
+}
+
+
+
+
+
+
+// AÑADIR NUEVA TRANSACCIÓN
+
+const buttonSubmit = document.querySelector('.btn-submit');
+const inputConcept = document.querySelector('#input-concept')
+const inputQuantity = document.querySelector('#input-quantity')
+
+function buttonClicked() {
+    localStorage.setItem("concept", inputConcept.value)
+    localStorage.setItem("quantity", inputQuantity.value)
+    console.log(inputConcept, inputQuantity)
+}
+
+
+
+buttonSubmit.addEventListener("click", buttonClicked)
+
